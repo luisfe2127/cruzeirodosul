6e12f530e148c53ed93dd10d5a724c59b9890fc9
Luís Felipe <luis.canarinho2126@gmail.com>
2024-03-19 13:45:31 -0300

    atualizando Formulaios

--- treinamento01/formularios01.php
+++ treinamento01/formularios01.php
@@ -1,11 +1,14 @@
+<?php
 
-header("Content-Type: text/html; charset=utf-8");
-
-echo “Dados recebidos do formulário!!!<br/><br/>”; 
-
-echo “Você digitou:<br />”;
-
-echo “Nome: “ .$_POST[“nome”].”<br />”;
+echo "Dados recebidos do formulário!!!<br/><br/>";
+echo "Você digitou:<br/>";
+echo "Nome: " .$_POST["nome"]."<br />";
 echo “Sexo: “ .$_POST[“sexo”].”<br />”;
-echo “Região: “ .$_POST[“regiao”]. <br />”;
-echo “Comente sobre o formulário: “ .$_POST[“coment”]. <br />”;
+echo “E-mail: “ .$_POST[“email”].”<br />”;
+echo “Assunto: “.$_POST[“assunto”].”<br />”;
+echo “Curso: “ .$_POST[“curso”].”<br />”;
+echo “Mensagem: “.$_POST[“mensagem”].”<br />”;
+
+?>
+
+
